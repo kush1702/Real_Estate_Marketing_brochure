3b672ec1c409a86812a78ef6495842b5db70c617
kush <budhirajakush7@gmaill.com>
2024-05-26 16:26:48 +0530

    improved style

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ function TextPromptForm() {
 
     return (
       <div className="container mt-5">
-        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Real Estate</span> Marketing Copy</h1>
+        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Real Estate</span> Marketing Brochure</h1>
         <FormComponent
           tone={tone}
           setTone={setTone}
